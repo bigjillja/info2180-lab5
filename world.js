7bafc6e31fce9ee4cbaf4be2e93ff955d650ebb9
bigjillja <officialmartinbartley@gmail.com>
2022-11-14 22:44:48 +0300

    Completed exercise 5

--- world.js
+++ world.js
@@ -1,20 +1,42 @@
 window.onload = function(){
     var lookupBtn = document.getElementById("lookup");
+    var cityBtn = document.getElementById("cities")
     var results = document.getElementById("result");
     var country = document.getElementById("country");
     var request = new XMLHttpRequest();
 
     lookupBtn.addEventListener('click', element);
+    cityBtn.addEventListener('click', element2);
 
     function element(e){
         e.preventDefault();
-        var url = "world.php?country=" + country.value;
+        var url = "world.php?country=" + country.value + "&context=countries";
         request.onreadystatechange = fetch;
         request.open('GET', url, true);
         request.send();
     }
 
+    function element2(e2){
+        e2.preventDefault();
+        var url = "world.php?cities=" + country.value + "&context=cities";
+        request.onreadystatechange = fetchcities;
+        request.open('GET', url, true);
+        request.send();
+    }
+
     function fetch(){
+        if (request.readyState === XMLHttpRequest.DONE){
+            if (request.status === 200){
+                var response = request.responseText;
+                results.innerHTML = response;
+            }
+            else{
+                results.innerHTML = "Error: This request can not be delivered. Please try again.";
+            }
+        }
+    }
+
+    function fetchcities(){
         if (request.readyState === XMLHttpRequest.DONE){
             if (request.status === 200){
                 var response = request.responseText;
--- world.php
+++ world.php
@@ -4,19 +4,36 @@ $username = 'lab5_user';
 $password = 'password123';
 $dbname = 'world';
 
-$country = $_GET['country'];
+$page= $_SERVER['REQUEST_URI'];
+$url=parse_url($page,PHP_URL_QUERY);
+parse_str($url,$param);
+$context= $param['context'];
+
+$country = filter_input(INPUT_GET, "country", FILTER_SANITIZE_STRING);
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
-$stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%';");
-$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+$countriesQuery = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%';");
+$citiesQuery= $conn->query("SELECT cities.name, cities.district, cities.population FROM countries join cities on cities.country_code = countries.code WHERE countries.name LIKE '%$country'"); 
 
-echo "<table><tr><th>Name</th><th>Continent</th><th>Independence Year</th><th>Head of State</th></tr>";
-foreach ($results as $row):
-  echo "<tr><td>".$row["name"]."</td><td>".$row["continent"]."</td><td>".$row["independence_year"]."</td><td>".$row["head_of_state"]."</td></tr>";
-endforeach;
-echo "</table>";
+$countriesResults = $countriesQuery->fetchAll(PDO::FETCH_ASSOC);
+$citiesResults = $citiesQuery->fetchAll(PDO::FETCH_ASSOC);
+
+
+if($context=="countries"){
+  echo "<table><tr><th>Name</th><th>Continent</th><th>Independence Year</th><th>Head of State</th></tr>";
+  foreach ($countriesResults as $row):
+    echo "<tr><td>".$row["name"]."</td><td>".$row["continent"]."</td><td>".$row["independence_year"]."</td><td>".$row["head_of_state"]."</td></tr>";
+  endforeach;
+  echo "</table>";
+}else{
+  unset($page,$url,$context);
+  $page= null;
+  $url= null;
+  $context= null;
+  echo "<table><tr><th>Name</th><th>District</th><th>Population</th></tr>";
+  foreach($citiesResults as $row):
+    echo "<tr><td>".$row["name"]."</td><td>".$row["district"]."</td><td>".$row["population"]."</td></tr>";
+  endforeach;
+  echo "</table>";
+}
+
 ?>
-
-
-
-
-
